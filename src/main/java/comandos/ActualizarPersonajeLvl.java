2d771f9e8fcad4b372d91b796d9e6832c76d4ee6
Oc Frutos <o_frutos@yahoo.com>
2017-10-08 20:58:24 -0300

    Auto stash before rebase of "origin/master"

--- src/main/java/comandos/ActualizarPersonajeLvl.java
+++ src/main/java/comandos/ActualizarPersonajeLvl.java
@@ -2,21 +2,26 @@ package comandos;
 
 import mensajeria.PaquetePersonaje;
 
+/**
+ * Clase que administra la actualización del personaje con respecto a su nivel.
+ * <br>
+ */
 public class ActualizarPersonajeLvl extends ComandosEscucha {
-
+	/**
+	 * 
+	 */
 	@Override
 	public void ejecutar() {
 		PaquetePersonaje paquetePersonaje = (PaquetePersonaje) gson.fromJson(cadenaLeida, PaquetePersonaje.class);
-		
+
 		juego.getPersonajesConectados().remove(paquetePersonaje.getId());
 		juego.getPersonajesConectados().put(paquetePersonaje.getId(), paquetePersonaje);
-		
+
 		if (juego.getPersonaje().getId() == paquetePersonaje.getId()) {
 			juego.actualizarPersonaje();
 			juego.getEstadoJuego().actualizarPersonaje();
+			juego.getCliente().subirDeNivel();
 			juego.getCliente().actualizarPersonaje(juego.getPersonajesConectados().get(paquetePersonaje.getId()));
 		}
-		
 	}
-
 }
